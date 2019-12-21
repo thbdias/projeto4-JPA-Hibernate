7949f3530756dfd0152cdcaef291304ca2eef3b4
thbd <thiagobalbino91@gmail.com>
2019-12-21 08:07:23 -0300

    criando a classe java HibernateUtil

---
+++ src/test/java/pos_java_maven_hibernate/TesteHibernate.java
@@ -0,0 +1,12 @@
+package pos_java_maven_hibernate;
+
+import org.junit.Test;
+
+public class TesteHibernate {
+
+	@Test
+	public void testeHibernateUtil(){
+		HibernateUtil.getEntityManager();
+	}
+	
+}
