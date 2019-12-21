7a8e5aef5273ac6e1f38c6831a56b44b316c3d05
thbd <thiagobalbino91@gmail.com>
2019-12-21 18:31:19 -0300

    criando dao generico

---
+++ src/main/java/dao/DaoGeneric.java
@@ -0,0 +1,21 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import pos_java_maven_hibernate.HibernateUtil;
+
+public class DaoGeneric<E> {
+	
+	private EntityManager entityManager = HibernateUtil.getEntityManager();
+	
+	
+	public void salvar(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(entidade);
+		transaction.commit();
+	}
+	
+
+}
--- src/test/java/pos_java_maven_hibernate/TesteHibernate.java
+++ src/test/java/pos_java_maven_hibernate/TesteHibernate.java
@@ -2,11 +2,24 @@ package pos_java_maven_hibernate;
 
 import org.junit.Test;
 
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
 public class TesteHibernate {
 
 	@Test
 	public void testeHibernateUtil(){
-		HibernateUtil.getEntityManager();
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		pessoa.setIdade(45);
+		pessoa.setLogin("teste");
+		pessoa.setNome("maria 2");
+		pessoa.setSenha("123");
+		pessoa.setSobrenome("rosario");
+		pessoa.setEmail("teste@teste.com");
+		
+		daoGeneric.salvar(pessoa);
 	}
 	
 }
