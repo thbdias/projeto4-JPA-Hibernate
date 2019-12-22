689c584eef2eca6df2a58a443a21be29f570a99c
thbd <thiagobalbino91@gmail.com>
2019-12-22 20:54:24 -0300

    criando relacionamento entre objetos e tabelas

--- src/main/java/model/UsuarioPessoa.java
+++ src/main/java/model/UsuarioPessoa.java
@@ -1,17 +1,18 @@
 package model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 
 @Entity
-@NamedQueries({
-	@NamedQuery(name = "UsuarioPessoa.todos", query = "select u from UsuarioPessoa u"),
-	@NamedQuery(name = "UsuarioPessoa.buscaPorNome", query = "select u from UsuarioPessoa u where u.nome = :nome") 
-})
+@NamedQueries({ @NamedQuery(name = "UsuarioPessoa.todos", query = "select u from UsuarioPessoa u"),
+		@NamedQuery(name = "UsuarioPessoa.buscaPorNome", query = "select u from UsuarioPessoa u where u.nome = :nome") })
 public class UsuarioPessoa {
 
 	@Id
@@ -23,9 +24,19 @@ public class UsuarioPessoa {
 	private String login;
 	private String senha;
 	private int idade;
+	@OneToMany(mappedBy = "pessoa")
+	private List<TelefoneUser> telefoneUsers;
+
 	
 	
-	
+	public List<TelefoneUser> getTelefoneUsers() {
+		return telefoneUsers;
+	}
+
+	public void setTelefoneUsers(List<TelefoneUser> telefoneUsers) {
+		this.telefoneUsers = telefoneUsers;
+	}
+
 	public int getIdade() {
 		return idade;
 	}
@@ -35,49 +46,49 @@ public class UsuarioPessoa {
 	}
 
 	public Long getId() {
-		return id;	
+		return id;
 	}
-	
+
 	public void setId(Long id) {
-		this.id = id;	
+		this.id = id;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getSobrenome() {
 		return sobrenome;
 	}
-	
+
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getLogin() {
 		return login;
 	}
-	
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
-	
+
 	public String getSenha() {
 		return senha;
 	}
-	
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
@@ -85,8 +96,10 @@ public class UsuarioPessoa {
 	@Override
 	public String toString() {
 		return "UsuarioPessoa [id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + ", email=" + email
-				+ ", login=" + login + ", senha=" + senha + ", idade=" + idade + "]";
+				+ ", login=" + login + ", senha=" + senha + ", idade=" + idade + ", telefoneUsers=" + telefoneUsers
+				+ "]";
 	}
 
 	
+
 }
